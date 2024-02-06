0527715ddcbd56708aef28f500081c8ef6f5ffc9
Clemens Banowski <officialeta09@gmail.com>
2024-02-06 19:19:05 +0100

    dashboard init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { readdirSync } from 'fs';
 
 @Module({
-  imports: [],
+  imports: [
+    ...readdirSync(join(__dirname, '..', 'public')).map((dir) =>
+      ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '..', 'public', dir),
+        serveRoot: join('/', dir),
+      }),
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Response } from 'express';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  dashboardRedirect(response: Response) {
+    response.redirect('/dashboard');
   }
 }
